5934a8c376e83f9b7d7c2dbd4cfb6d56beb33620
alloubetver18 <98085689+alloubetver18@users.noreply.github.com>
2024-02-08 09:12:28 +0100

    commit-20240208

--- js/digifantasy.js
+++ js/digifantasy.js
@@ -3,9 +3,15 @@ function nightModeGeneral(){
     if(ruta.includes('css/nightmodeGlobal.css')){
         document.getElementById('global').href = 'css/global.css';
         document.getElementById('searchIcon').setAttribute("src", "img/Iconssearch.svg");
+        if(document.getElementById('iconsRating')!=null || document.getElementById('iconsRating')!=undefined){
+            document.getElementById('iconsRating').setAttribute("src", "img/IconsRating.svg");
+        }
         
     }else{
         document.getElementById('global').href = 'css/nightmodeGlobal.css';
         document.getElementById('searchIcon').setAttribute("src", "img/IconsSearchNight.svg");
+        if(document.getElementById('iconsRating')!=null || document.getElementById('iconsRating')!=undefined){
+            document.getElementById('iconsRating').setAttribute("src", "img/IconsRatingNight.svg");
+        }
     }
 }
